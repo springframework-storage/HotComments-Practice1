8420b006c5b47f8921fea825a67463e3676226ae
mingooddev <mingood92@gmail.com>
2018-04-30 15:03:02 +0900

    fix: Comment userId, postId 속성 순서 변경

--- src/main/java/campus/hackday/dto/Comment.java
+++ src/main/java/campus/hackday/dto/Comment.java
@@ -6,8 +6,8 @@ import lombok.Data;
 public class Comment {
 
   private int id;
-  private int userId;
   private int postId;
+  private int userId;
   private String content;
   private int pstReactCount;
   private int ngtReactCount;
