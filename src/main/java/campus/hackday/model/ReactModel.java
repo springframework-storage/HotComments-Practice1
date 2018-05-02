27a59913f2bfaa357cddc3361666d77302830fcb
mingooddev <mingood92@gmail.com>
2018-05-02 21:31:58 +0900

    feat: 공감/비공감 모델 추가 공감/비공감 데이터를 담을 ReactModel 을 추가합니다.

---
+++ src/main/java/campus/hackday/model/ReactModel.java
@@ -0,0 +1,13 @@
+package campus.hackday.model;
+
+import lombok.Data;
+
+@Data
+public class ReactModel {
+
+  private int id;
+  private int commentId;
+  private int userId;
+  private int postId;
+
+}
